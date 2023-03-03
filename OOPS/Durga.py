151b1de9e362aa43cd9a23ad9a9fe60305e52b3c
Kavin-Velusamy <kavinvelusamy01@gmail.com>
2023-03-03 09:58:57 -0500

    changes

--- OOPS/Durga.py
+++ OOPS/Durga.py
@@ -1,24 +1,24 @@
 
 
-# creating multiple object in simple program
+# # creating multiple object in simple program
 
-class student :
-    def __init__(self,name,no):
-        self.name=name
-        self.no=no
+# class student :
+#     def __init__(self,name,no):
+#         self.name=name
+#         self.no=no
         
-    def details(self):
-        print("my name is",self.name)
-        print("my no is",self.no)
+#     def details(self):
+#         print("my name is",self.name)
+#         print("my no is",self.no)
        
-s=student ('erevv',34)
-print(s.name,s.no) 
-s.details()
+# s=student ('erevv',34)
+# print(s.name,s.no) 
+# s.details()
 
-s2=student('eiopov',2354)
-print(s2.name,s2.no) 
-s2.details()
+# s2=student('eiopov',2354)
+# print(s2.name,s2.no) 
+# s2.details()
 
-s3=student('egdfbh',24)
-print(s3.name,s3.no) 
-s3.details()
+# s3=student('egdfbh',24)
+# print(s3.name,s3.no) 
+# s3.details()
