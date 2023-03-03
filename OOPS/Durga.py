5ae5c62905492c1f8538bc7cecf81f2c4fded7e4
Kavin-Velusamy <kavinvelusamy01@gmail.com>
2023-03-03 09:51:52 -0500

    changes

--- OOPS/Durga.py
+++ OOPS/Durga.py
@@ -1,2 +1,16 @@
 
 
+
+
+class student :
+    def __init__(self,name,no):
+        self.name=name
+        self.no=no
+        
+    def details(self):
+        print("my name is",self.name)
+        print("my no is",self.no)
+       
+s=student ('erevv',34)
+print(s.name,s.no) 
+s.details()
